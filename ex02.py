acd6acafee2b27179b2ae19fa01d8a0782ad291a
Reid Atcheson <reid.atcheson@gmail.com>
2023-03-19 11:29:35 -0500

    added convergence rates

--- ex02.py
+++ ex02.py
@@ -2,12 +2,16 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import scipy.sparse.linalg as spla
 
 
-k=1
+
+
+#Order of GMRES polynomial
+k=4
 
 #First fit polynomial (without constant term) to the constant 1
-eigs=np.array([0.2,0.3,0.4,0.5,1.0])
+eigs=np.array([-0.5,-0.3,0.2,0.3,0.4,0.5,1.0])
 V=np.zeros((len(eigs),k))
 for i in range(1,k+1):
     V[:,i-1]=eigs**i
@@ -32,12 +36,13 @@ for i in range(1,k+1):
     conv+=c[i-1]*eigs**i
 
 
-convrate=np.linalg.norm(1.0-conv)
-print(1.0-conv)
-print(convrate)
+convrate=max(abs(1.0-conv))
 
 
 
+
+
+plt.subplot(2,1,1)
 plt.plot(xs,1.0-ys)
 #Put markers on the eigenvalues
 plt.plot(eigs,np.zeros(len(eigs)),'o')
@@ -58,8 +63,44 @@ plt.legend(['Polynomial','Eigenvalues','Convergence region'])
 #label the box
 #plt.text(0.0,1.0,"Region of convergence",ha='center',va='bottom')
 
-#Place computed convergence rate in bottom of plot as text for reference
-plt.text(0.0,-1.5,f"Convergence rate: {convrate:.2f}",ha='center',va='bottom')
+#Place computed convergence rate in top of plot as text for reference
+plt.text(0.0,1.5,f"Convergence rate bound: {convrate:.2e}",ha='center',va='bottom')
+#plt.text(0.0,-1.5,f"Convergence rate bound: {convrate:.2f}",ha='left',va='bottom')
 
+
+
+
+
+seed=2397
+rng=np.random.default_rng(seed)
+D=np.diag(eigs)
+Q,_=np.linalg.qr(rng.random((len(eigs),len(eigs))))
+A=Q @ D @ Q.T
+b=Q @ np.ones(len(eigs))
+
+resl=[]
+def callback(xk):
+    resl.append(np.linalg.norm(A @ xk - b))
+
+
+spla.gmres(A,b,callback=callback,restart=k,callback_type='x')
+
+#Put GMRES convergence into a subplot
+plt.subplot(2,1,2)
+
+predicted=[]
+for i in range(len(resl)):
+    if convrate<1.0:
+        predicted.append(np.linalg.norm(b)*(convrate**i))
+    else:
+        predicted.append(np.linalg.norm(b))
+
+#Make room for plot title of bottom subplot
+plt.subplots_adjust(hspace=0.5)
+plt.title("GMRES Convergence")
+plt.semilogy(resl)
+plt.semilogy(predicted)
+plt.legend(['Actual GMRES residual','Residual bound'])
+plt.xlabel("Iteration")
 
 plt.savefig('ex02.svg')
